93a7d7068f7ff38ea34383e826caafb334e8dd43
amir maleki <amirmaleki21physics@gmail.com>
2021-12-27 13:02:17 +0330

    question box component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import { question } from "./Question";
 
 function App() {
+  const [showCurrnetAnswer, setShowCurrentAnswer] = useState(false);
+  const [score, setScore] = useState(0);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [showScore, setShowScore] = useState(false);
+  const ansewerBtnHandler = (isCorrect) => {
+    const nextQuestion = currentQuestion + 1;
+    {
+      isCorrect && setScore(score + 1);
+    }
+    {
+      nextQuestion < question.length
+        ? setCurrentQuestion(nextQuestion)
+        : setShowScore(true);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="box-question">
+        {showScore ? (
+          <div className="score">
+            امتیاز شما {score} از {question.length}
+          </div>
+        ) : (
+          <>
+            <div className="question-section">
+              <div className="detail">
+                <span> سوال </span>
+                <span className="question-number"> {currentQuestion + 1}</span>
+                <span> از </span>
+                <span>{question.length} </span>
+              </div>
+              <div className="question">
+                {question[currentQuestion].questionText}
+              </div>
+            </div>
+            <div className="answer-section">
+              {question[currentQuestion].questionOption.map(
+                (questionOption) => {
+                  return (
+                    <button
+                      onClick={() =>
+                        ansewerBtnHandler(questionOption.isCorrect)
+                      }
+                      className="answer-button"
+                      // {questionOption.isCorrect &&  style={{borderColor: "green"}}
+                    >
+                      {questionOption.answerText}
+                    </button>
+                  );
+                }
+              )}
+            </div>
+          </>
+        )}
+      </div>
     </div>
   );
 }
