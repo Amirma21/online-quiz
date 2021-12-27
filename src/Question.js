04d1a4c54bccb59047913ec8353b807676c4a15a
amir maleki <amirmaleki21physics@gmail.com>
2021-12-27 12:56:07 +0330

    list of question

---
+++ src/Question.js
@@ -0,0 +1,46 @@
+export  const question = [
+    {
+      questionText:
+        " عامل بیماری سرماخوردگی چیست؟",
+      questionOption: [
+        { answerText: "باکتری", isCorrect: false },
+        { answerText: "ویروس ", isCorrect: true },
+        { answerText: "میکروب ", isCorrect: false },
+      ],
+    },
+    {
+      questionText: " بعد از مغز پیچیده ترین عضو بدن انسان کدام است ؟",
+      questionOption: [
+        { answerText: "قلب", isCorrect: false },
+        { answerText: "کبد", isCorrect: true },
+        { answerText: "کلیه", isCorrect: false },
+        { answerText: "معده", isCorrect: false },
+      ],
+    },
+    {
+      questionText: " فرآیند داخل هسته خورشید که باعث تولید انرژی و نور می شود چه نام دارد؟",
+      questionOption: [
+        { answerText: "فیوژن  ", isCorrect: true },
+        { answerText: " اَستروژن  ", isCorrect: false },
+        { answerText: "کتوژن", isCorrect: false },
+      
+      ],
+    },
+    {
+      questionText: " داروی آنتی بیوتیک را برای چه دلیلی می خوریم؟  ",
+      questionOption: [
+        { answerText: "سرما خوردگی  ", isCorrect: false },
+        { answerText: " عفونت و باکتری  ", isCorrect: true },
+        { answerText: "ویروس", isCorrect: false },
+      ],
+    },  {
+      questionText: "کدامیک از اعداد زیر با هم متفاوت می باشد؟ ",
+      questionOption: [
+        { answerText: "3", isCorrect: false },
+        { answerText: "4", isCorrect: true },
+        { answerText: "2", isCorrect: false },
+        { answerText: "7", isCorrect: false },
+      ],
+    },
+    
+  ];
